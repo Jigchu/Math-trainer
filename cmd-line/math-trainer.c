960635d3d9e640e67db8b92bba90530d77217a66
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-03-03 23:28:19 +0800

    Revert "Making gitignore work"
    This reverts commit 09ec807e8a08535e4ac0b845ec5485bf08be2c54.

--- cmd-line/math-trainer.c
+++ cmd-line/math-trainer.c
@@ -1,13 +1,8 @@
-#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Prevent getopt to print message on stderr
-opterr = 0;
-
 int main(int argc, char const *argv[])
 {
-    
     
     return 0;
 }
