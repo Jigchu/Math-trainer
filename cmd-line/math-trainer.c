09ec807e8a08535e4ac0b845ec5485bf08be2c54
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-03-03 23:23:42 +0800

    Making gitignore work

--- cmd-line/math-trainer.c
+++ cmd-line/math-trainer.c
@@ -1,8 +1,13 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// Prevent getopt to print message on stderr
+opterr = 0;
+
 int main(int argc, char const *argv[])
 {
+    
     
     return 0;
 }
