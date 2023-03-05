9464c674456917dfcba50c422e29d596ab75944e
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-03-05 21:07:56 +0800

    Completed basic parsing and added some longopts

--- cmd-line/math-trainer.c
+++ cmd-line/math-trainer.c
@@ -1,5 +1,6 @@
 #include <getopt.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,19 +8,20 @@
 // Macro for array size
 #define ARRAY_SIZE(arr)     (sizeof(arr) / sizeof((arr)[0]))
 
-// Make getopt not write anything to stderr
-int opterr = 0;
-
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
+    // Make getopt not write anything to stderr
+    opterr = 0;
+
     // Opening options file
-    FILE *options = fopen("options.txt", "r");
+    FILE *options = fopen("config/options.txt", "r");
     
     if (options == NULL)
     {
         printf("Could not open options.txt\n");
         printf("Perhaps you misplaced it? (It should be in the config folder)\n");
-        return 1;
+
+        exit(1);
     }
 
     // Gets short options
@@ -32,8 +34,10 @@ int main(int argc, char const *argv[])
     if (optstr == NULL)
     {
         printf("Could not allocate memory for options\n");
+
         fclose(options);
-        return 2;
+
+        exit(2);
     }
 
     fread(optstr, sizeof(char), len, options);
@@ -41,17 +45,85 @@ int main(int argc, char const *argv[])
 
     // Long options
     static struct option longopts[] = {
-        {"archive", no_argument, NULL, 256},
-        {"remember", no_argument, NULL, 255},
+        {"archive", no_argument, NULL, 257},
+        {"remember", no_argument, NULL, 258},
     };
 
-    // Input parsing
-    uint8_t *opts = malloc(len + ARRAY_SIZE(longopts));
+    int optlen = len + ARRAY_SIZE(longopts);
+    int long_index = -1;
 
+    // Input parsing 
+
+    // Getting the options
+    int *opts = malloc((optlen + 1) * sizeof(int));                 /*Stores all the options in argv*/
+    int i = 0;                                                      /*Iterable variable*/
+    
+    while (true)
+    {
+        opts[i] = getopt_long(argc, argv, optstr, longopts, &long_index);
+
+        // Checks for invalid option
+        if (opts[i] == '?')
+        {
+            printf("Invalid option\n");
+
+            free(optstr);
+            free(opts);
+            fclose(options);
+
+            exit(3);
+        }
+
+        // Checks for the end of options
+        if (opts[i] == -1)
+        {
+            break;
+        }    
+        
+        i++;
+    }
+
+    // Checks if no options are inputted
+    if (i == 0)
+    {
+        printf("No options inputted\n");
+        
+        free(optstr);
+        free(opts);
+        fclose(options);
+
+        exit(4);
+    }
+
+    // Getting all other values
+
+    // Prevent integer overflow
+    if (atoll(argv[optind]) > INT_MAX)
+    {
+        printf("Integer overflow: please input something less or equal to %d\n", INT_MAX);
+        
+        free(optstr);
+        free(opts);
+        fclose(options);
+
+        exit(5);
+    }
+
+    int q_num = atoi(argv[optind]);
+
+    // The equation generation part
+    /*
+    .
+    .
+    .
+    */
+
+    // Archival system
 
     // Frees all memory
     free(optstr);
+    free(opts);
     fclose(options);
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
