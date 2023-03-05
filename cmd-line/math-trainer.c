4418a20fa3b79e8d8a65297ed529dabee7b5b539
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-03-05 19:42:50 +0800

    Removed executable

--- cmd-line/math-trainer.c
+++ cmd-line/math-trainer.c
@@ -1,7 +1,11 @@
 #include <getopt.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// Macro for array size
+#define ARRAY_SIZE(arr)     (sizeof(arr) / sizeof((arr)[0]))
 
 // Make getopt not write anything to stderr
 int opterr = 0;
@@ -14,6 +18,7 @@ int main(int argc, char const *argv[])
     if (options == NULL)
     {
         printf("Could not open options.txt\n");
+        printf("Perhaps you misplaced it? (It should be in the config folder)\n");
         return 1;
     }
 
@@ -27,6 +32,7 @@ int main(int argc, char const *argv[])
     if (optstr == NULL)
     {
         printf("Could not allocate memory for options\n");
+        fclose(options);
         return 2;
     }
 
@@ -35,8 +41,17 @@ int main(int argc, char const *argv[])
 
     // Long options
     static struct option longopts[] = {
-
+        {"archive", no_argument, NULL, 256},
+        {"remember", no_argument, NULL, 255},
     };
+
+    // Input parsing
+    uint8_t *opts = malloc(len + ARRAY_SIZE(longopts));
+
+
+    // Frees all memory
+    free(optstr);
+    fclose(options);
 
     return 0;
 }
