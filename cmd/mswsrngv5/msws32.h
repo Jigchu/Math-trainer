4fe82e2bf4cece5abe377fcf61193e0a0f890f7b
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-04-05 11:47:10 +0800

    added rng dependancy

---
+++ cmd/mswsrngv5/msws32.h
@@ -0,0 +1,104 @@
+/**************************************************************************\
+*                                                                          *
+*  Middle-Square Weyl Sequence Random Number Generator (32-bit output)     *
+*                                                                          *
+*  msws32() - returns a 32 bit unsigned int [0,0xffffffff]                 *
+*                                                                          *
+*  The state vector consists of three 64 bit words:  x, w, and s           * 
+*                                                                          *
+*  x - random output [0,0xffffffff]                                        *
+*  w - Weyl sequence (period 2^64)                                         *
+*  s - an odd constant                                                     *
+*                                                                          *
+*  The Weyl sequence is added to the square of x.  The middle is extracted *
+*  by shifting right 32 bits:                                              *
+*                                                                          *
+*  x *= x; x += (w += s); return x = (x>>32) | (x<<32);                    *
+*                                                                          *
+*  The constant s should be set to a random 64-bit pattern.  The utility   *
+*  init_rand_digits in init.h may be used to initialize the constant.      *
+*  This utility generates constants with different hexadecimal digits.     *
+*  This assures sufficient change in the Weyl sequence on each iteration   *
+*  of the RNG.                                                             *
+*                                                                          *
+*  Note:  This version of the RNG includes an idea proposed by             *
+*  Richard P. Brent (creator of the xorgens RNG).  Brent suggested         *
+*  adding the Weyl sequence after squaring instead of before squaring.     *
+*  This provides a basis for uniformity in the output.                     *
+*                                                                          *
+*  Copyright (c) 2014-2022 Bernard Widynski                                *
+*                                                                          *
+*  This code can be used under the terms of the GNU General Public License *
+*  as published by the Free Software Foundation, either version 3 of the   *
+*  License, or any later version. See the GPL license at URL               *
+*  http://www.gnu.org/licenses                                             *
+*                                                                          *
+\**************************************************************************/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "../random.h"
+
+uint64_t seed[25000] = {
+   #include "seed.h"
+};
+
+uint64_t x = seed[22389], w = seed[22389], s = seed[22389];
+
+// My own local function to dynamically change the seed
+inline static void msws32_s(void)
+{
+   uint8_t bit32 = rand() % 2;
+
+   if (bit32)
+   {
+      jump_ahead(rand());
+
+      uint32_t new_seed = msws_uint(SEEDN_MAX);
+   }
+   else if (!bit32)
+   {
+      jump_ahead2(rand()));
+
+      uint64_t new_seed = msws_ull(SEEDN_MAX);
+   }
+
+   x = w = s = seed[new_seed];
+
+   return;
+}
+
+inline static uint32_t msws32(void)
+{
+   x *= x; x += (w += s); return x = (x>>32) | (x<<32);
+
+}
+
+/**************************************************************************\
+*                                                                          *
+*  Jump ahead m elements in the Weyl sequence (32-bit output)              *
+*                                                                          *
+*  jump_ahead(m)                                                           *
+*                                                                          *
+*  The jump ahead is performed by setting w += (m * s);                    *
+*                                                                          *
+*  This allows for the creation of separate non-overlapping streams of     *
+*  data.  One might call jump_ahead(n*trillion) for example to create a    *
+*  stream for processor n. Also, x is randomized so that the streams       *
+*  produced will be relatively random.  The first outputs of each stream   *
+*  taken as a set should be random data.  We used the method from the      * 
+*  counter-based squares RNG to do this.  Several rounds of squaring are   *
+*  performed and the result is stored in x.                                *
+*                                                                          *
+\**************************************************************************/
+
+inline static void jump_ahead(uint64_t m)
+{
+   uint64_t y;
+
+   x = w += (m * s); y = w + s;
+   x = x*x + w; x = (x>>32) | (x<<32);
+   x = x*x + y; x = (x>>32) | (x<<32);
+   x = x*x + w; x = (x>>32) | (x<<32);
+
+}
---
+++ cmd/mswsrngv5/msws64.h
@@ -0,0 +1,113 @@
+/**************************************************************************\
+*                                                                          *
+*  Middle-Square Weyl Sequence Random Number Generator (64-bit output)     *
+*                                                                          *
+*  msws64() - returns a 64 bit unsigned int [0,0xffffffffffffffff]         *
+*                                                                          *
+*  This 64-bit version performs the same computations as the 32-bit        *
+*  version.  Half of the computations are on x1, w1, and s1.  The          *
+*  rest are on x2, w2, and s2.  Instruction-level parallelism (ILP)        *
+*  on modern CPUs makes it possible to perform some of the computations    *
+*  in parallel.  For this reason, this version is faster than calling      *
+*  the 32-bit version twice.                                               *
+*                                                                          *
+*  The constants s1 and s2 should be set to a random 64-bit patterns.      *
+*  The utility init_rand_digits in init.h may be used to initialize the    *
+*  constants.  This utility generates constants with different hexadecimal *
+*  digits.  This assures sufficient change in the Weyl sequence on each    *
+*  iteration of the RNG.                                                   *
+*                                                                          *
+*  Note:  This version of the RNG includes an idea proposed by             *
+*  Richard P. Brent (creator of the xorgens RNG).  Brent suggested         *
+*  adding the Weyl sequence after squaring instead of before squaring.     *
+*  This provides a basis for uniformity in the output.                     *
+*                                                                          *
+*  Copyright (c) 2014-2022 Bernard Widynski                                *
+*                                                                          *
+*  This code can be used under the terms of the GNU General Public License *
+*  as published by the Free Software Foundation, either version 3 of the   *
+*  License, or any later version. See the GPL license at URL               *
+*  http://www.gnu.org/licenses                                             *
+*                                                                          *
+\**************************************************************************/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "../random.h"
+
+uint64_t seed[25000] = {
+   #include "seed.h"
+};
+
+uint64_t x1 = seed[21928], w1 = seed[21928], s1 = seed[21928];
+uint64_t x2 = seed[21928], w2 = seed[21928], s2 = seed[12398];
+
+inline static void msws64_s(void)
+{
+   uint8_t bit32 = rand() % 2;
+
+   if (bit32)
+   {
+      jump_ahead(rand());
+
+      uint32_t new_seed1 = msws_uint(SEEDN_MAX);
+      uint32_t new_seed2 = msws_uint(SEEDN_MAX);
+   }
+   else if (!bit32)
+   {
+      jump_ahead2(rand());
+
+      uint64_t new_seed1 = msws_ull(SEEDN_MAX);
+      uint64_t new_seed2 = msws_ull(SEEDN_MAX);
+   }
+
+   x1 = w1 = s1 = seed[new_seed1];
+   x2 = w2 = s2 = seed[new_seed2];
+
+   return;
+}
+
+inline static uint64_t msws64() {
+
+   uint64_t xx;
+
+   x1 *= x1; xx = x1 += (w1 += s1); x1 = (x1 >> 32) | (x1 << 32);
+   x2 *= x2;      x2 += (w2 += s2); x2 = (x2 >> 32) | (x2 << 32);
+
+   return xx ^ x2;
+
+}
+
+/**************************************************************************\
+*                                                                          *
+*  Jump ahead m elements in the Weyl sequence (64-bit output)              *
+*                                                                          *
+*  jump_ahead2(m)                                                          *
+*                                                                          *
+*  The jump ahead is performed by setting w1 += (m * s1);                  *
+*                                         w2 += (m * s2);                  *
+*                                                                          *
+*  This allows for the creation of separate non-overlapping streams of     *
+*  data.  One might call jump_ahead2(n*trillion) for example to create a   *
+*  stream for processor n. Also, x1 and x2 are randomized so that the      *
+*  streams produced will be relatively random.  The first outputs of each  *
+*  stream taken as a set should be random data.  We used the method from   * 
+*  the counter-based squares RNG to do this.  Several rounds of squaring   *
+*  are performed and the results is stored in x1 and x2.                   *
+*                                                                          *
+\**************************************************************************/
+
+inline static void jump_ahead2(uint64_t m) {
+
+   uint64_t y1, y2;
+
+   x1 = w1 += (m * s1); y1 = w1 + s1;
+   x2 = w2 += (m * s2); y2 = w2 + s2;
+   x1 = x1*x1 + w1; x1 = (x1>>32) | (x1<<32);
+   x2 = x2*x2 + w2; x2 = (x2>>32) | (x2<<32);
+   x1 = x1*x1 + y1; x1 = (x1>>32) | (x1<<32);
+   x2 = x2*x2 + y2; x2 = (x2>>32) | (x2<<32);
+   x1 = x1*x1 + w1; x1 = (x1>>32) | (x1<<32);
+   x2 = x2*x2 + w2; x2 = (x2>>32) | (x2<<32);
+
+}
